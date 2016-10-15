3d5213ed79a53866945df4c3e3808611af2b51aa
eduardohmrodrigues <eduardohmrodrigues@gmail.com>
2016-10-15 11:40:14 -0300

    Adding new editor scripts
    now we have a wizard that clone objects with a given translation between then;
    Was added too a wizard that select all objects with a given tag;
    was added too an asset post processor that converts every texture put on the scripts folder to a sprite;

---
+++ Assets/Editor/SpriteProcessor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class SpriteProcessor : AssetPostprocessor {
+    
+    void OnPostprocessTexture(Texture2D texture)
+    {
+        string lowerCaseAssetPath = assetPath.ToLower();
+        bool isInSpritesDirectory = (lowerCaseAssetPath.IndexOf("/sprites/") != -1);
+
+
+        if (isInSpritesDirectory)
+        {
+            TextureImporter importer = (TextureImporter)assetImporter;
+            importer.textureType = TextureImporterType.Sprite;
+        }
+    }
+
+}
---
+++ Assets/Editor/Wizards/CloneObjects.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class CloneObjects : ScriptableWizard {
+
+    [Tooltip("The Object to Clone")]
+    public GameObject objectToClone;
+
+    [Tooltip("The number of clones")]
+    public int numberOfCopies = 0;
+
+    [Tooltip("The distance of the clones in the x y z axis")]
+    public Vector3 clonesTranslationDistance;
+
+    [Tooltip("If is the first instance then the first clone is created in the object initial position, else the first clone will be created already in the new position")]
+    public bool isFirstInstace = false;
+
+    [MenuItem("My Tools/Clone Objects")]
+    static void CloneObjectsWizard()
+    {
+        ScriptableWizard.DisplayWizard<CloneObjects>("Clone Objects", "Clone");
+    }
+
+
+    void OnWizardCreate()
+    {
+        if(objectToClone)
+        {
+            int initialTranslation = isFirstInstace ? 1 : 0;
+
+            for(int i=0; i<numberOfCopies; i++)
+            {
+                Object clone = Instantiate(
+                    objectToClone,
+                    new Vector3(
+                        ((i + initialTranslation) * clonesTranslationDistance.x) + objectToClone.transform.position.x,
+                        ((i + initialTranslation) * clonesTranslationDistance.y) + objectToClone.transform.position.y,
+                        ((i + initialTranslation) * clonesTranslationDistance.z) + objectToClone.transform.position.z
+                        ),
+                    Quaternion.identity);
+
+                clone.name = objectToClone.name + (" Clone") + i;
+            }
+        }
+    }
+
+}
